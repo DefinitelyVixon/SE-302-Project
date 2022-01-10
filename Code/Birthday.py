f0bb69af7e1075f3b2f3cf2302b0d37c5b9a66a2
Ozan Sahin <ozansahin.ac@gmail.com>
2022-01-10 13:15:50 +0300

    Birthday class initial version added

---
+++ Code/Birthday.py
@@ -0,0 +1,8 @@
+class Birthday:
+    def __init__(self, day: int, month: int, year: int):
+        self.day = day
+        self.month = month
+        self.year = year
+
+    def date_to_string(self):
+        return f"{self.day}-{self.month}-{self.year}"
