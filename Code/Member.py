8ba040bc444176d73d83b7ea095cd5e2ac6c7801
Ozan Sahin <ozansahin.ac@gmail.com>
2022-01-11 11:59:49 +0300

    gender attribute added check level() method will change later

--- Code/Member.py
+++ Code/Member.py
@@ -2,12 +2,13 @@ from Birthday import Birthday
 
 
 class Member:
-    def __init__(self, name: str, surname: str, age: int,
+    def __init__(self, name: str, surname: str, age: int, gender: str,
                  member_layer_level: int, day=0, month=0, year=0):
         self.name = name
         self.surname = surname
         self.age = age
         self.birthday = Birthday(day, month, year)
+        self.gender = gender
         self.member_layer_level = member_layer_level
 
     def check_level(self):
