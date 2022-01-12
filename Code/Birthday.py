3700e99e8bc698ab29bea62d831c0c7ae1115a6c
Ege Altıok <egealtiok@gmail.com>
2022-01-12 17:14:58 +0300

    Change: More dynamic constructor and overridden default str attribute.

--- Code/Birthday.py
+++ Code/Birthday.py
@@ -1,8 +1,13 @@
 class Birthday:
-    def __init__(self, day: int, month: int, year: int):
-        self.day = day
-        self.month = month
-        self.year = year
+    def __init__(self, *args):
+        if len(args) == 1 and isinstance(args[0], str):
+            args = args[0].split("/")
+        self.day = args[0]
+        self.month = args[1]
+        self.year = args[2]
 
     def date_to_string(self):
         return f"{self.day}/{self.month}/{self.year}"
+
+    def __str__(self):
+        return self.date_to_string()
