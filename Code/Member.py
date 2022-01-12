ad56271766a2397633fcad632199d2e11f13c483
Ege Altıok <egealtiok@gmail.com>
2022-01-12 17:16:50 +0300

    Change: More dynamic constructor and overridden default str attribute.
    Remove: member_layer_level and check_level()

--- Code/Member.py
+++ Code/Member.py
@@ -2,15 +2,23 @@ from Birthday import Birthday
 
 
 class Member:
-    def __init__(self, name: str, surname: str, age: int, gender: str,
-                 member_layer_level: int, day=0, month=0, year=0):
+    def __init__(self, name: str, surname: str, age: int, birthday=None, gender=None):
+        # member_layer_level: int,
         self.name = name
         self.surname = surname
         self.age = age
-        self.birthday = Birthday(day, month, year)
+        if birthday is None or isinstance(birthday, Birthday):
+            self.birthday = birthday
+        elif isinstance(birthday, str):
+            self.birthday = Birthday(birthday)
         self.gender = gender
-        self.member_layer_level = member_layer_level
+        # self.member_layer_level = member_layer_level
 
-    def check_level(self):
-        return self.member_layer_level
+    # def check_level(self):
+    #     return self.member_layer_level
 
+    def full_name(self):
+        return f'{self.name} {self.surname}'
+
+    def __str__(self):
+        return self.full_name()
