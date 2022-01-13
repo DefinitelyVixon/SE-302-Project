d0f7fd1e7f5ebb325636e1d5ce8190085fe09468
Ege Altıok <egealtiok@gmail.com>
2022-01-13 15:02:12 +0300

    Fix: Some python module problem caused by importing Birthday.py to Member.py
    Remove: member_layer_level and check_level()

--- Code/Member.py
+++ Code/Member.py
@@ -1,9 +1,8 @@
-from Birthday import Birthday
+from Code.Birthday import Birthday
 
 
 class Member:
     def __init__(self, name: str, surname: str, age: int, birthday=None, gender=None):
-        # member_layer_level: int,
         self.name = name
         self.surname = surname
         self.age = age
@@ -12,10 +11,6 @@ class Member:
         elif isinstance(birthday, str):
             self.birthday = Birthday(birthday)
         self.gender = gender
-        # self.member_layer_level = member_layer_level
-
-    # def check_level(self):
-    #     return self.member_layer_level
 
     def full_name(self):
         return f'{self.name} {self.surname}'
